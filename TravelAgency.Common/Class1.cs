3fec9f5d50c4e0ab4db012d026ecf5f7b38b468f
haiber bedoya <haiefra@hotmail.com>
2019-10-31 20:57:44 -0500

    Web project creation

---
+++ TravelAgency.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TravelAgency.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ TravelAgency.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TravelAgency.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
